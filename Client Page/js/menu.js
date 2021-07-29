380430cef1911d9b227c4182f4b23bd03e9d13f2
OussamaDhraief <oussema.dhraief@gmail.com>
2021-07-29 13:50:30 +0100

    commit

--- Client Page/js/menu.js
+++ Client Page/js/menu.js
@@ -3,12 +3,26 @@ const mq = window.matchMedia("(max-width: 992px)");
 if (mq.matches) {
     const mainNav = document.querySelector(".navright");
     const menuIcon = document.querySelector("#menu-icon");
-
+    mainNav.style.display = "none";
+    
     menuIcon.addEventListener("click", function () {
-        if (mainNav.style.display != "grid") {
+        if (mainNav.style.display == "none") {
             mainNav.style.display = "grid";
         } else {
-            mainNav.style.display = "None";
+            mainNav.style.display = "none";
         }
+
+        
     });
 }
+
+mq.addEventListener("change", (e) => {
+    if (e.matches == false) {
+        const mainNav = document.querySelector(".navright");
+        mainNav.style.display = "flex";
+
+    } else {
+        const mainNav = document.querySelector(".navright");
+        mainNav.style.display = "none";
+    }
+});
