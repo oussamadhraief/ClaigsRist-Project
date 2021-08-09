407fdff02abad11db4e5a39c4402d867c9375977
OussamaDhraief <oussema.dhraief@gmail.com>
2021-08-09 17:17:11 +0100

    commit

--- Client Page/js/auth.js
+++ Client Page/js/auth.js
@@ -358,6 +358,7 @@ document.querySelector("#google-sign-in").addEventListener("click", () => {
       var user = result.user;
       console.log(user.uid);
         let userRef = ref("Users/" + user.uid);
+        console.log(userRef);
       ref("Users/" + user.uid).once("value", (snapshot) => {
           console.log("this is inside the ref");
         if(!snapshot.authMethods.exists()){
