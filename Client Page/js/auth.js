51e4e24c82849d10bc2a50c357c3a47056b7b8d3
OussamaDhraief <oussema.dhraief@gmail.com>
2021-08-09 23:46:53 +0100

    c

--- Client Page/js/auth.js
+++ Client Page/js/auth.js
@@ -203,6 +203,86 @@ auth.onAuthStateChanged(user => {
         });
     } else {
         setupUI();
+        document.querySelector("#modal-account").innerHTML = `
+                    <h4>My account</h4><br>
+                    <a class="close-modal" onClick="handleCloseModal('modal-account')" href="#">x</a>
+                    <div id="account-form">
+                        <div id="profile-pic">
+                            <img src="https://i2.wp.com/proseawards.com/wp-content/uploads/2015/08/no-profile-pic.png"
+                                id="user-pic" alt="profile picture">
+                            <div id="picture-edit">
+                                <label for="profilepic">Edit your profile picture</label>
+                                <input type="file" id="profilepic" accept="image/png, image/gif, image/jpeg">
+                                <a href="#" onClick="handleRemoveButton()" id="remove-picture">Remove picture</a>
+                                <a id="save-picture" onClick="handleSavePicture()" disabled>Save</a>
+                            </div>
+                        </div>
+        
+        
+        
+                        <div id="account-email" class="input-field">
+                            <p class="temp" style="display: grid;">Edit your email.</p>
+        
+                            <a href="#" class="editanchor" style="display: grid;"
+                                onClick="handleEditButton('account-email')"><img class="editicon" src="edit-form.png"
+                                    alt="edit"></a>
+        
+                            <input style="display: none;" id="acc-email" type="text" class="edit-email" name="edit-email"
+                                required>
+        
+                            <button onClick="saveButtonAppearance('account-email')" id="save-email" style="display: none;"
+                                class="save-info">Save</button>
+        
+                            <a href="#" style="display: none;" class="cancel-button"
+                                onClick="handleCancelButton('account-email')">cancel</a>
+        
+                        </div>
+        
+        
+        
+                        <div id="account-password" class="input-field">
+                            <p class="temp" style="display: grid;">Edit your password.</p>
+        
+                            <a href="#" class="editanchor" style="display: grid;"
+                                onClick="handleEditButton('account-password')"><img class="editicon" src="edit-form.png"
+                                    alt="edit"></a>
+        
+                            <input style="display: none;" id="acc-password" type="password" class="edit-email"
+                                name="edit-password" required>
+        
+                            <button onClick="saveButtonAppearance('account-password')" id="save-password" style="display: none;"
+                                class="save-info">Save</button>
+        
+                            <a href="#" style="display: none;" class="cancel-button"
+                                onClick="handleCancelButton('account-password')">cancel</a>
+                        </div>
+        
+        
+        
+                        <div id="account-bio" class="input-field">
+                            <p class="temp" style="display: grid;">Edit your bio.</p>
+        
+                            <a href="#" class="editanchor" style="display: grid;" onClick="handleEditButton('account-bio')"><img
+                                    class="editicon" src="edit-form.png" alt="edit"></a>
+        
+                            <input style="display: none;" id="acc-bio" type="text" class="edit-email" name="edit-bio" required>
+        
+                            <button onClick="saveButtonAppearance('account-bio')" id="save-bio" style="display: none;"
+                                class="save-info">Save</button>
+        
+                            <a href="#" style="display: none;" class="cancel-button"
+                                onClick="handleCancelButton('account-bio')">cancel</a>
+                        </div>
+                        <div id="PS">
+                <img src="attention.png" alt="attention-icon" width="30px" height="30px">
+                <p id="attention"> IMPORTANT : logging in with google / facebook only makes signing up and signing in easier and creates a ClaigsRist account that is completely detached 
+                from your google/facebook account. 
+                Changing your email or password or any other type of information (photo, bio ... ) below will only affect your ClaigsRist account
+                and will not change your login information or any other type of information in your google/facebook account. </p>
+                </div>
+                    </div>
+                    `;
+                
     }
 });
 
@@ -351,7 +431,7 @@ function handleCloseModal(id) {
 
 let googleProvider = new firebase.auth.GoogleAuthProvider();
 
-console.log("gg");
+console.log("gggg111g");
 
 document.querySelector("#google-sign-in").addEventListener("click", () => {
     firebase.auth().useDeviceLanguage();
