2ab5f5dd95708a9707db1d15a1ed37a305b60aec
OussamaDhraief <oussema.dhraief@gmail.com>
2021-08-09 13:47:08 +0100

    commit

--- Client Page/js/auth.js
+++ Client Page/js/auth.js
@@ -342,7 +342,7 @@ function handleCloseModal(id) {
 
 let googleProvider = new firebase.auth.GoogleAuthProvider();
 
-console.log("g");
+console.log("gg");
 
 document.querySelector("#google-sign-in").addEventListener("click", () => {
     googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
