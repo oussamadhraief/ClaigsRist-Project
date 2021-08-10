8346f6f043b9a35eb4d8d78c94ce8872cc5be8fd
OussamaDhraief <oussema.dhraief@gmail.com>
2021-08-11 00:55:06 +0100

    commited

--- Client Page/js/menu.js
+++ Client Page/js/menu.js
@@ -10,20 +10,24 @@ if (mq.matches) {
 
     menuIcon.addEventListener("click", function () {
         if (mainNav.style.display == "none") {
-            mainNav.classList.add("animate__animated", "animate__fadeInDown");
+            mainNav.classList.add("animate__animated", "animate__slideInRight");
             search.style.width = "90%";
             search.style.margin = "10px";
             search.style.marginTop = "0px";
             mainNav.style.display = "grid";
             menuIcon.src = "close-icon.png";
             menuIcon.style.borderRadius = "50%";
+            // document.querySelector("#top-anchor-navright").setAttribute("style",`margin-top: ${document.querySelector("#nav").offsetHeight -3}px !important`);
+            mainNav.style.marginTop = document.querySelector("#nav").offsetHeight-5 +"px";
+            document.querySelector("#top-anchor-navright").classList.add("menu-icon-clicked");
+            
 
         } else {
-            mainNav.classList.remove("animate__animated", "animate__fadeInDown");
+            mainNav.classList.remove("animate__animated", "animate__slideInRight");
             mainNav.style.display = "none";
             search.style.marginTop = "10px";
             menuIcon.src = "menu-icon.png";
-
+            document.querySelector("#top-anchor-navright").classList.remove("menu-icon-clicked");
         }
 
 
